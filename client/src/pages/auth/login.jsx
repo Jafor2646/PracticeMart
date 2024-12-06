267777f28036b37e883861ea7fcd6080b055ba2c
Jafor Mohammad <jaformd7492@gmail.com>
2024-12-06 17:09:09 +0600

    changed some packet version and login register page is created and routing is added

---
+++ client/src/pages/auth/login.jsx
@@ -0,0 +1,12 @@
+
+
+
+function AuthLogin() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
+
+export default AuthLogin;   
---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function AuthRegister() {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+}
+
+export default AuthRegister;
