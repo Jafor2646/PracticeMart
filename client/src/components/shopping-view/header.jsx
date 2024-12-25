627aff9448d076871eae9494c90edc567ea64f5f
Jafor Mohammad <jaformd7492@gmail.com>
2024-12-26 01:24:40 +0600

    Shopping listing for user is started

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,8 +1,50 @@
-function ShoppingHeader() {
+import { HousePlug, Menu } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import { useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "@/config";
+
+function MenuItems(){
   return (
-    <div>
-      <h1>Shopping List</h1>
-    </div>
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center lg:flex-row gap-6">
+      {
+        shoppingViewHeaderMenuItems.map((item) => {
+          <Link key={item.id} to={item.path} className="text-sm font-medium">{item.label}</Link>
+      })
+    }
+    </nav>
+  );
+}
+
+function ShoppingHeader() {
+  const {isAuthenticaed} = useSelector((state) => state.auth);
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+            <Menu className="h-6 w-6"/>
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+        {
+          isAuthenticaed ? <div></div> : null
+        }
+      </div>
+    </header>
   );
 }   
 
