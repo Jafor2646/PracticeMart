e494eafc58bc78c08849d1952af1a5fc0ad5a7b1
Jafor Mohammad <jaformd7492@gmail.com>
2025-01-03 16:48:06 +0600

    Product show complete

--- client/src/components/shopping-view/product-tile.jsx
+++ client/src/components/shopping-view/product-tile.jsx
@@ -1,3 +1,4 @@
+import { brandOptionsMap, categoryOptionsMap } from "@/config";
 import { Badge } from "../ui/badge";
 import { Card, CardContent, CardFooter } from "../ui/card";
 
@@ -5,14 +6,14 @@ import { Card, CardContent, CardFooter } from "../ui/card";
 
 function ShoppingProductTile({ product }) {
   return (
-    <div className="product-tile">
-      <Card className="w-full max-w-sm mx-auto">
+
+      <Card className="rounded-xl w-full max-w-sm mx-auto">
         <div>
             <div className="relative">
                 <img 
                     src={product?.image}
                     alt={product?.title}
-                    className="w-full h-[300px] object-cover rounded-t-lg"
+                    className="w-full h-[300px] object-cover rounded-t-xl"
                 />
                 {
                     product?.salePrice > 0 ?
@@ -21,14 +22,14 @@ function ShoppingProductTile({ product }) {
                 <CardContent className="p-4">
                     <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
                     <div className="flex justify-between items-center mb-2"> 
-                        <span className="text-sm text-muted-foreground">{product?.category}</span>         
-                        <span className="text-sm text-muted-foreground">{product?.brand}</span>         
+                        <span className="text-[16px] text-muted-foreground">{categoryOptionsMap[product?.category]}</span>         
+                        <span className="text-[16px] text-muted-foreground">{brandOptionsMap[product?.brand]}</span>         
                     </div>
                     <div className="flex justify-between items-center mb-2"> 
-                        <span className={`${product?.salePrice > 0 ? 'line-through' : ''} text-lg font-semibold text-primary`}>{product?.price}</span>
+                        <span className={`${product?.salePrice > 0 ? 'line-through' : ''} text-lg font-semibold text-primary`}>${product?.price}</span>
                         {
                             product?.salePrice > 0 ? 
-                            <span className="text-lg font-semibold text-primary">{product?.salePrice}</span> : null
+                            <span className="text-lg font-semibold text-primary">${product?.salePrice}</span> : null
                         }         
                     </div>
                 </CardContent>
@@ -38,7 +39,7 @@ function ShoppingProductTile({ product }) {
             </div>
         </div>
       </Card>
-    </div>
+
   );
 
 }
--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,13 +1,20 @@
 import ProductFilter from "@/components/shopping-view/filter";
+import ShoppingProductTile from "@/components/shopping-view/product-tile";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuRadioGroup, DropdownMenuRadioItem } from "@/components/ui/dropdown-menu";
 import { sortOptions } from "@/config";
+import { fetchAllFilteredProducts } from "@/store/shop/products-slice";
 import { ArrowUpDown } from "lucide-react";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function ShoppingListing() {
   const dispatch = useDispatch();
+
+  const { productList } = useSelector(state=>state.shopProducts);
   //fetch list of products
+  useEffect(()=>{
+    dispatch(fetchAllFilteredProducts());
+  }, [dispatch]);
 
     return (
       <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
@@ -16,7 +23,7 @@ function ShoppingListing() {
           <div className="p-4 border-b flex items-center justify-between">
             <h2 className="text-lg font-extrabold">All Products</h2>
             <div className="flex items-center gap-3">
-              <span className="text-muted-foreground">10 Products</span>
+              <span className="text-muted-foreground">{productList.length} Products</span>
               <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <button variant="outline" size="sm"  className="flex items-center gap-1">
@@ -34,9 +41,12 @@ function ShoppingListing() {
             </DropdownMenu>
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-
-
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+             {
+              productList && productList.length > 0 ?
+              productList.map(productItem => <ShoppingProductTile product={productItem}/>) : null
+             }         
+          
           </div>
         </div>
       </div>
