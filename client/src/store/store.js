b5a261404a7f8c8642cd1d0dd8a15559096ebef8
Jafor Mohammad <jaformd7492@gmail.com>
2025-07-10 14:35:10 +0600

    Some part of review is done

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,6 +7,7 @@ import shoppingProductSlice from "./shop/products-slice";
 import shoppingCartSlice from "./shop/cart-slice";
 import shoppingAddressSlice from "./shop/address-slice";
 import shoppingOrderSlice from "./shop/order-slice";
+import shoppingReviewSlice from "./shop/review-slice";
 
 const store = configureStore({
     reducer: {
@@ -18,7 +19,8 @@ const store = configureStore({
         shopProducts : shoppingProductSlice,
         shopCart : shoppingCartSlice,
         shopAddress : shoppingAddressSlice,
-        shopOrder: shoppingOrderSlice
+        shopOrder: shoppingOrderSlice,
+        shopReview: shoppingReviewSlice
     }   
 });
 
--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ const shopProductsRouter = require('./routes/shop/products-routes');
 const shopCartRouter = require('./routes/shop/cart-routes');
 const shopAddressRouter = require('./routes/shop/address-routes');
 const shopOrderRouter = require('./routes/shop/order-routes');
+const shopReviewRouter = require('./routes/shop/review-routes');
+
+
 mongoose.connect(
     "mongodb://localhost:27017/express-mongo"
 ).then(() => console.log("Connected to MongoDB")
@@ -42,5 +45,6 @@ app.use('/api/shop/products', shopProductsRouter);
 app.use('/api/shop/cart', shopCartRouter);
 app.use('/api/shop/address', shopAddressRouter);
 app.use('/api/shop/order', shopOrderRouter);
+app.use('/api/shop/review', shopReviewRouter);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
